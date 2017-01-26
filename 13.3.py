4d3a4ef66efb50e3ffc6005624b9a3c4391e74a5
Celia Irene Martin Hernandez <martinc@gamma38.aulas.gsyc.urjc.es>
2017-01-26 12:09:39 +0100

    X-Serv-Python-Multiplicaejercicio tabla de multiplicar

---
+++ 13.3.py
@@ -0,0 +1,7 @@
+
+for num1 in range(1,11):
+    print("\nTabla del", num1)
+    print ("------------")
+    for num2 in range (1,11):
+        sol = num1 * num2
+        print(num1, "por", num2, "es",sol)
